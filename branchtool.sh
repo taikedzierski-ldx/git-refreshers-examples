8bebb32ef1a80a5f02b0ecf084abc77969e4a6fc
Tai Kedzierski <tai.kedzierski@lumiradx.com>
2020-08-07 12:25:20 +0100

    Turn hello example into branch tool

---
+++ branchtool.sh
@@ -0,0 +1,35 @@
+#! bash
+
+## Branch tool
+##
+## branchtool.sh { log | graph | count }
+
+checkhelp() {
+    if [[ "$*" =~ --help ]]; then
+        grep -E '^##' "$0"
+        exit
+    fi
+}
+
+main() {
+    checkhelp "$@"
+
+    local action
+    action="$1"; shift
+    case "$action" in
+        log)
+            git log --oneline
+            ;;
+        graph)
+            git log --oneline --graph --decorate=short --all
+            ;;
+        count)
+            local lines
+            lines="$(git log --oneline | wc -l)"
+
+            echo "This branch has $lines commits."
+            ;;
+    esac
+}
+
+main "$@"
